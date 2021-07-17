d96128b130e6c9a15d5ef927c0a4b606950ed576
Aysan Isayo <aisayo@icloud.com>
2021-07-17 15:00:09 -0700

    variables solution

--- index.js
+++ index.js
@@ -1,9 +1,53 @@
-// Creating Variables 
+// Creating Variables
+
+// define a variable 'fahrenheit' using 'var' and set it to 90
+    console.log("hoisted?", fahrenheit); // returns undefined
+    var fahrenheit = 90;
+    console.log("after initialization", fahrenheit); // returns 90
+
+// define 'fahrenheit' again using 'var' and set it to 100
+    var fahrenheit = 100;
+    console.log("redeclared", fahrenheit);
+
+// define a variable 'fahrenheit' using 'let' and set it to 90
+    console.log("hoisted?", fahrenheit); 
+    let fahrenheit = 90;
+    console.log("after initialization", fahrenheit); 
+
+// define 'fahrenheit' again using 'let' and set it to 100, can we?
+    let fahrenheit = 100;
+    console.log("redeclared", fahrenheit);
+
+// define a variable 'fahrenheit' using 'const' and set it to 90
+    const fahrenheit = 90
+
+// define 'fahrenheit' again using 'const' and set it to 100, what happens?
+    const fahrenheit = 100
 
 // Variable Scopes
 
-// Defining functions
+// Global Scope
 
-// Executing functions
+let globalVariable = "I am in the global scope";
 
-// Call Stack and Job Queue
+// Function Scope
+
+function whatsTheScope() {
+  let functionVariable = "I am inside of a function";
+}
+
+// // Block Scope
+
+function whatsTheScope() {
+  if (true) {
+    let blockVariable = "I am inside of a block";
+  }
+}
+
+// // Defining functions
+
+// // Executing functions
+
+// // Call Stack and Job Queue
+
+
